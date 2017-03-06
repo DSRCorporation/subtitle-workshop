c5f87319103f9682ffb63975324a2cfa52dc6cc3
Alexander Kolesov <kolesov.alex.s@gmail.com>
2017-03-06 16:15:48 +0300

    Playback pause performs before media loading.

--- Forms/formMain.pas
+++ Forms/formMain.pas
@@ -12513,17 +12513,17 @@ begin
 
   case previewSelected of
     psVideo     : begin
-                    if (previewFilename <> '') and not Player.Initialized then
-                      LoadVideo;
                     if WaveformAdapter.Displayer.IsNotPaused then
                       WaveformAdapter.PlayPause;
+                    if (previewFilename <> '') and not Player.Initialized then
+                      LoadVideo;
                     EnableVPCtrls(Player.Initialized);
                   end;
     psWaveform  : begin
-                    if (previewFilename <> '') and not WaveformAdapter.Displayer.IsPeakDataLoaded then
-                      LoadWaveform;
                     if Player.Initialized and Playing then
                       Pause;
+                    if (previewFilename <> '') and not WaveformAdapter.Displayer.IsPeakDataLoaded then
+                      LoadWaveform;
                     UpdateWaveformVolume;
                   end;
     psNone      : begin
